220428171e9a18f4fb354d97defa4a0e565cbb66
Endericedragon Fu <fuze1007@163.com>
2024-09-23 22:47:45 +0800

    安置了run-rupta.sh脚本，方便分析

---
+++ run-rupta.sh
@@ -0,0 +1,7 @@
+export LD_LIBRARY_PATH=$(rustc --print sysroot)/lib:$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
+# export PTA_LOG=info
+cargo clean
+# /home/endericedragon/repos/rupta/target/release/cargo-pta pta --release -- --entry-func main --dump-call-graph cg.dot
+cargo-pta pta --release -- --dump-call-graph cg.dot
+cat cg.dot | dot -Tsvg -o cg.svg
